9ca854ea31a937a3a8ef119976c294fa01038ed4
Rafael González <astrorafael@gmail.com>
2022-06-06 23:59:47 +0200

    adding health checks

--- run.sh
+++ run.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 podman pod create --name azotea -p 8624:8624
-podman run --pod azotea -d --name azotake --restart=always localhost/acquisition:0.1.0
+podman run -d --name indiserver --pod azotea ---restart=on-failure \
+--health-interval 1m --health-timeout 5s --health-start-period 2m \
+--health-cmd 'curl -I --fail http://localhost:8624 || exit 1'  \
+localhost/acquisition:0.1.0
+
