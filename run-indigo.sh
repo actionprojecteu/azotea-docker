cf5278391f4bfe471b88e91ea5b9489ec045fe24
Rafael Gonzalez <astrorafael@yahoo.es>
2022-06-07 16:11:51 +0200

    updating run indiogo

--- run-indigo.sh
+++ run-indigo.sh
@@ -2,5 +2,7 @@
 
 podman pod create --name azotea -p 7624:7624
 podman run -ti --name indigo --pod azotea --restart=on-failure \
+--privileged -v /dev/bus/usb:/dev/bus/usb \
+--mount type=tmpfs,destination=/tmp \
 localhost/indigo:0.1.0
 
